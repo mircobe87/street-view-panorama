c08d17d3188a726b8fbae9749bad8e9a7f493efa
Mirco Bertelli <mircobe87@gmail.com>
2015-01-12 01:42:41 +0100

    modifiche essenziali:
     - script bash rinominato;
     - pagina web ridisegnata;
     - lo script bash adesso avvia il server HTTP locale, apre la pagina web
       e attende l'inserimento, da parte dell'utente, del 'panoid'.

---
+++ street-view-panorama.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+python -m SimpleHTTPServer &> /dev/null &
+xdg-open "http://localhost:8000"
+
+echo "Inderisci l'ID del panorama:"
+read panoid
+
+#panoid="$1"
+
+mkdir panorama
+cd panorama
+
+curl http://cbk0.google.com/cbk?output=tile\&panoid=$panoid\&zoom=5\&x=[0-25]\&y=[0-12] -o "#2-#1-tile.jpg"
+
+for f in $( ls ?-* ); do
+	mv "$f" "0$f";
+done
+
+for f in $( ls ??-?-* ); do
+	mv "$f" "${f:0:3}${f/??-/0}";
+done
+
+montage -geometry 64x64 -tile 26x13 *-tile.jpg pano.jpg
+cp pano.jpg ../
+
+cd ..
+
+xdg-open pano.jpg
+pkill -f "python -m SimpleHTTPServer"
+
+exit 0
